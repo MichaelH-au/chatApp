6d4934d696eeeb40d7a2db8a02ec70a8baccbbc7
Michael <huangjkau@gmail.com>
2019-03-20 15:08:39 +1100

    First message send successfully

--- chatapp/src/containers/Test/index.js
+++ chatapp/src/containers/Test/index.js
@@ -1,17 +1,62 @@
 import React, {Component} from 'react';
 
 class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            receivedMessage:'',
+            message: ''
+
+        }
+        this.socket = new WebSocket('ws://localhost:8088/ws')
+    }
+    componentDidMount() {
+        if (window.WebSocket) {
+            this.socket.onopen = function () {
+                console.log('connect success')
+            }
+            this.socket.onclose = function () {
+                console.log('connect close')
+            }
+            this.socket.onerror = function () {
+                console.log('connect error')
+            }
+            this.socket.onmessage = function (e) {
+                this.setState({
+                    receivedMessage:e.data
+                })
+                console.log('get message')
+            }.bind(this)
+
+
+        }
+    }
+    sendMsg() {
+        console.log(this.state.message)
+        this.socket.send(this.state.message)
+    }
+
+    changeHendler(key, value) {
+        this.setState({
+            [key]:value
+        })
+    }
+
     render() {
         return (
-            <div>
-                <div>Send message</div>
-                <input type="text" name="msgContent"/>
-                <button className="btn btn-success">Send</button>
+            <div className="d-flex justify-content-around align-items-center flex-column" style={{height:"100vh"}}>
+                <div>
+                    <div>Send message</div>
+                    <input type="text" name="msgContent" value={this.state.message} onChange={(e) => this.changeHendler('message', e.target.value)}/>
+                    <button className="btn btn-success" onClick={() => this.sendMsg()}>Send</button>
+                </div>
 
-
-                <div>Send message</div>
-                <input type="text" name="msgContent"/>
-                <button className="btn btn-success">Send</button>
+                <div>
+                    <div>receive message</div>
+                    <div>{this.state.receivedMessage}</div>
+                    {/*<input type="text" name="msgContent"/>*/}
+                    {/*<button className="btn btn-success">Send</button>*/}
+                </div>
             </div>
         );
     }
